bbe3f915942eb799d9fab97c89388a2500b9a95f
Adam Olszewski <adamolszewski96@gmail.com>
2023-12-06 15:57:24 +0100

    pc version add

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 8080;
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'home.html'))
 });
 
 
